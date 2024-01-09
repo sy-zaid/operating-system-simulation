5e5fa24a5e0ffdd242156c43b60048a05c484ea6
SyedZaid <syedmzaid.99@gmail.com>
2024-01-10 00:36:44 +0500

    doc for application.py

--- windows/application.py
+++ windows/application.py
@@ -1,28 +1,31 @@
 import subprocess
 
+
+
 class ApplicationOpener:
-    
     """
     Choose an Application
     1. Google Chrome
     2. Microsoft Edge
     3. Task Manager
     Enter number 1/2/3:
+
     input-parameters:
             - choice: To choose between the applications 1/2/3
-            - subprocess: It creates a process and runs it        
+
+    subprocess: It creates a process and runs it.
     """
     def open_application(self):
         print("Choose an Application\n1. Google Chrome\n2. Microsoft Edge\n3. Task Manager")
-        choice = input("Enter number 1/2/3: ")
-        if choice == "1":
-            subprocess.run([r'C:\Program Files\Google\Chrome\Application\chrome.exe'])
+        choice = input("Enter number 1/2/3: ")  # User will input the respective number
+        if choice == "1": # Will compare the input
+            subprocess.run([r'C:\Program Files\Google\Chrome\Application\chrome.exe']) # Will run the application
         elif choice == "2":
             subprocess.run([r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'])
         elif choice == "3":
             subprocess.run([r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\System Tools'])
         else:
-            print("Invalid choice. Please enter 1, 2, or 3.")
+            print("Invalid choice. Please enter 1, 2, or 3.") # Will print if incorrect input given
 
 # ob = ApplicationOpener()
 # ob.open_application()
